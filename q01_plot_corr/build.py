08d56cf5bb2b203839da2bc400b7b7a2fbb16ea7
vp999 <patilvish@gmail.com>
2018-02-04 12:59:24 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -6,3 +6,13 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your solution here:
+import seaborn as sns
+from matplotlib import pyplot as plt
+plt.switch_backend('agg')
+
+def plot_corr(df,size=11):
+    plt.figure(figsize=(size,6))
+    sns.heatmap(df.corr(), cmap='YlOrRd')
+    print xticks(), yticks()
+
+#plot_corr(data)
